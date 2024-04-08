9f12287a230c058fed528b39fdd4fc05b1a7c948
e133706 <nidura.murali@mastercard.com>
2024-04-08 22:35:49 +0530

    firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqp2nv7wmvexKTaQaxvFLwR9HvO7HRRxg",
+  authDomain: "customers-admin-portal.firebaseapp.com",
+  projectId: "customers-admin-portal",
+  storageBucket: "customers-admin-portal.appspot.com",
+  messagingSenderId: "117318854777",
+  appId: "1:117318854777:web:d8728eac269e6ad2bd0ca8",
+  measurementId: "G-1WZCDXX5R2"
+};
+
+export default firebaseConfig;
+
